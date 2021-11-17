ccc9b4eccd7fbda56ba4d2ef905574c133784081
scottbromander <scottbromander@gmail.com>
2021-11-17 09:32:21 -0600

    update to final versions

--- d.ts
+++ d.ts
@@ -4,20 +4,32 @@
 
 abstract class BaseController {}
 
-interface IEmailService {}
+interface IEmailService {
+  sendMail();
+}
 
-class MailGunService implements IEmailService {}
-
-// Bad!
-class CreateUserController extends BaseController {
-  // we're limiting ourselves to a particlar concrete class.
-  private emailService: MailGunService; // <- concretion
-
-  constructor(emailService: MailGunService) {
-    super();
-    this.emailService = emailService;
+class MailGunService implements IEmailService {
+  sendMail() {
+    // whatever we want to send the email
   }
 }
+
+class MailChimp implements IEmailService {
+  sendMail() {
+    // whatever we want to send the email
+  }
+}
+
+// Bad!
+// class CreateUserController extends BaseController {
+//   // we're limiting ourselves to a particlar concrete class.
+//   private emailService: MailGunService; // <- concretion
+
+//   constructor(emailService: MailGunService) {
+//     super();
+//     this.emailService = emailService;
+//   }
+// }
 
 // // SUPER BAD!!!
 // class CreateUserController extends BaseController {
@@ -32,11 +44,15 @@ class CreateUserController extends BaseController {
 ////////
 
 // Good :)
-// class CreateUserController extends BaseController {
-//   private emailService: IEmailService; // <- abstraction
-//
-//   constructor(emailService: IEmailService) {
-//     super();
-//     this.emailService = emailService;
-//   }
-// }
+class CreateUserController extends BaseController {
+  private emailService: IEmailService; // <- abstraction
+
+  constructor(emailService: IEmailService) {
+    super();
+    this.emailService = emailService;
+  }
+
+  emailCustomer() {
+    //   this.emailService.sendMail("Hello");
+  }
+}
--- i.ts
+++ i.ts
@@ -1,41 +1,41 @@
 // Interface Segregation Principle
 // A Client should not be forced to depend upon interfaces and methods that they do not use.
 
-interface IEmployee {
-  reportHours();
-  calculatePay();
-  terminateEmployee();
-}
-
-class HRManager implements IEmployee {
-  reportHours() {}
-  calculatePay() {}
-  terminateEmployee() {}
-}
-
-class CompensationManager implements IEmployee {
-  reportHours() {}
-  calculatePay() {}
-}
-
-//////////
-
-// interface ICompensationManager {
+// interface IEmployee {
 //   reportHours();
 //   calculatePay();
-// }
-
-// interface IManager {
 //   terminateEmployee();
 // }
 
-// class HRManager implements ICompensationManager, IManager {
+// class HRManager implements IEmployee {
 //   reportHours() {}
 //   calculatePay() {}
 //   terminateEmployee() {}
 // }
 
-// class CompensationManager implements ICompensationManager {
+// class CompensationEmployee implements IEmployee {
 //   reportHours() {}
 //   calculatePay() {}
 // }
+
+//////////
+
+interface ICompensationManager {
+  reportHours();
+  calculatePay();
+}
+
+interface IManager {
+  terminateEmployee();
+}
+
+class HRManager implements ICompensationManager, IManager {
+  reportHours() {}
+  calculatePay() {}
+  terminateEmployee() {}
+}
+
+class CompensationEmployee implements ICompensationManager {
+  reportHours() {}
+  calculatePay() {}
+}
